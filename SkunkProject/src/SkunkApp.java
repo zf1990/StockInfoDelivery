c1b5a64b6affc9dda94614d9110bc076b3a9aa7a
MinhNguyenUST <nguy1766@stthomas.edu>
2020-03-06 10:42:49 -0600

    Create new Main for SkunkApp and simple roll

---
+++ SkunkProject/src/SkunkApp.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.introcs.StdOut;
+
+/*Main App*/
+public class SkunkApp {
+	
+	public static void main(String[] args) {
+		StdOut.println("Welcome to Skunk App");
+		
+		//Create new Dice instance 
+		Dice dice = new Dice();
+		dice.roll(); //invoke roll
+		
+		//Print out last roll
+		StdOut.println(dice.getLastRoll());
+	}
+}
