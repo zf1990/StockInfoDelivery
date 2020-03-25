8feceeeaeff520158f921648e5d5396c838fedda
Matthew Nickoloff <nick1030@sttomas.edu>
2020-03-24 20:14:36 -0500

    TestTurn Class add to Master

---
+++ SkunkProject/test/TestTurn.java
@@ -0,0 +1,28 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+//Class for testing Turn -
+
+public class TestTurn {
+	
+	@Test
+	public void test_turn_se_score()
+	{
+		Turn test_score = new Turn();
+		test_score.setScore(25);
+		int expected = 25;
+		assertThat(test_score.getScore(), is(expected));
+	}
+	
+	@Test
+	public void test_turn_set_score30()
+	{
+		Turn test_score = new Turn();
+		test_score.setScore(30);
+		int expected = 30;
+		assertThat(test_score.getScore(), is(expected));
+	}
+
+}
