758714cc4f541d7875702baae83e8e835ea4ecc5
Matthew Nickoloff <nick1030@sttomas.edu>
2020-05-10 15:20:17 -0500

    Edited output "Currently registerred stock" to "Currently registered stock"

--- FinancialPull/src/Main.java
+++ FinancialPull/src/Main.java
@@ -1,5 +1,5 @@
-//https://financialmodelingprep.com/developer/docs/#Stock-Price --API
-//https://mkyong.com/webservices/jax-rs/restfull-java-client-with-java-net-url/ --Code
+///https://financialmodelingprep.com/developer/docs/#Stock-Price --API
+///https://mkyong.com/webservices/jax-rs/restfull-java-client-with-java-net-url/ --Code
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -124,7 +124,7 @@ public class Main {
 		while(true)
 		{
 			//Showing current stock
-			StdOut.println("Currently registeredd stock:");
+			StdOut.println("Currently registered stock:");
 			String user_stock_list = (user.getUserStock() != "") ? user.getUserStock() : "None";
 			StdOut.println(user_stock_list);
 			
