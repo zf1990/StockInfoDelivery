9fb6d760d151aaa32475477fe01c8a0e59438170
Matthew Nickoloff <nick1030@sttomas.edu>
2020-05-15 11:41:42 -0500

    Adding Code MR Metric Breakdown to Delivery

---
+++ FinancialPull/codemr/FinancialPull/html/main_report/htmlx/modeldata/sunburst_data.js
@@ -0,0 +1,132 @@
+function EQ_GET_DATA(){ 
+	  var ret = {
+"name": " FinancialPull", "value": "512", 
+"prmetrics":{"5":1,"6":1,"7":1,"8":1,"9":2,"10":1,"11":1}
+,
+"prmetricvalues":{"5":0,"6":9,"7":0,"8":512,"9":11,"10":1,"11":61}
+,
+"children": [ {
+"name": "", "value": "512", 
+"pmetrics":{"4":2,"12":1,"13":1,"14":2,"3":1,"1":1,"0":1,"6":2,"8":2,"2":1,"15":1,"16":1}
+,
+"pmetricvalues":{"4":2,"12":3,"13":0,"14":9,"3":1,"1":1,"0":1,"17":0.0,"6":9,"18":0.0,"8":512,"19":1.0,"2":1,"15":0,"16":105}
+,
+"children": [ {
+"name": "EmailSender","key": "k","value": "60", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":1,"0":2,"27":1,"28":1,"29":3,"30":4,"31":1,"32":2,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":2,"8":2,"2":1,"3":2,"1":1}
+,
+"metricvalues":{"20":1,"21":0,"22":10,"23":0.0,"24":49,"25":0,"26":1,"0":2,"27":7,"28":20,"29":0.733,"30":0.917,"31":0,"32":0.607,"33":0,"34":18,"35":0,"36":1,"16":9,"37":0,"4":2,"8":60,"2":1,"3":2,"1":1}
+}
+,{
+"name": "UserController","key": "l","value": "109", 
+"metrics":{"20":2,"21":1,"22":1,"23":1,"24":2,"25":1,"26":1,"0":2,"27":1,"28":2,"29":2,"30":4,"31":1,"32":1,"33":2,"34":1,"35":1,"36":1,"16":2,"37":1,"4":2,"8":2,"2":2,"3":1,"1":2}
+,
+"metricvalues":{"20":6,"21":1,"22":6,"23":0.0,"24":102,"25":0,"26":1,"0":2,"27":13,"28":70,"29":0.628,"30":0.803,"31":0,"32":0.596,"33":6,"34":37,"35":0,"36":0,"16":23,"37":0,"4":2,"8":109,"2":2,"3":1,"1":2}
+}
+,{
+"name": "StockAttributes","key": "o","value": "17", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":2,"0":2,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":1,"8":1,"2":1,"3":1,"1":2}
+,
+"metricvalues":{"20":0,"21":0,"22":0,"23":0.0,"24":0,"25":0,"26":2,"0":2,"27":0,"28":0,"29":0.0,"30":0.0,"31":16,"32":0.0,"33":0,"34":0,"35":0,"36":0,"16":0,"37":0,"4":1,"8":17,"2":1,"3":1,"1":2}
+}
+,{
+"name": "User","key": "p","value": "53", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":1,"0":2,"27":1,"28":1,"29":1,"30":3,"31":1,"32":2,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":2,"8":2,"2":1,"3":2,"1":1}
+,
+"metricvalues":{"20":1,"21":0,"22":4,"23":0.0,"24":47,"25":0,"26":1,"0":2,"27":15,"28":20,"29":0.5,"30":0.733,"31":1,"32":0.637,"33":1,"34":4,"35":1,"36":0,"16":18,"37":0,"4":2,"8":53,"2":1,"3":2,"1":1}
+}
+,{
+"name": "UI","key": "s","value": "101", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":2,"25":1,"26":1,"0":1,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":2,"8":2,"2":1,"3":1,"1":1}
+,
+"metricvalues":{"20":2,"21":0,"22":0,"23":0.0,"24":100,"25":0,"26":1,"0":1,"27":8,"28":22,"29":0.0,"30":0.0,"31":0,"32":0.417,"33":0,"34":17,"35":0,"36":2,"16":16,"37":0,"4":2,"8":101,"2":1,"3":1,"1":1}
+}
+,{
+"name": "CSVWriter","key": "q","value": "14", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":1,"0":1,"27":1,"28":1,"29":5,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":1,"8":1,"2":1,"3":1,"1":1}
+,
+"metricvalues":{"20":0,"21":0,"22":1,"23":0.0,"24":12,"25":0,"26":1,"0":1,"27":1,"28":5,"29":1.0,"30":0.0,"31":0,"32":0.333,"33":0,"34":3,"35":1,"36":0,"16":4,"37":0,"4":1,"8":14,"2":1,"3":1,"1":1}
+}
+,{
+"name": "Main","key": "m","value": "74", 
+"metrics":{"20":1,"21":2,"22":1,"23":1,"24":2,"25":1,"26":1,"0":2,"27":1,"28":2,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":2,"8":2,"2":1,"3":1,"1":2}
+,
+"metricvalues":{"20":4,"21":2,"22":0,"23":0.0,"24":71,"25":0,"26":1,"0":2,"27":0,"28":81,"29":0.0,"30":0.25,"31":2,"32":0.333,"33":4,"34":21,"35":3,"36":0,"16":18,"37":0,"4":2,"8":74,"2":1,"3":1,"1":2}
+}
+,{
+"name": "CSVReader","key": "n","value": "18", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":1,"0":1,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":1,"8":1,"2":1,"3":1,"1":1}
+,
+"metricvalues":{"20":0,"21":0,"22":1,"23":0.0,"24":16,"25":0,"26":1,"0":1,"27":2,"28":6,"29":0.0,"30":0.0,"31":0,"32":0.25,"33":0,"34":4,"35":0,"36":0,"16":5,"37":0,"4":1,"8":18,"2":1,"3":1,"1":1}
+}
+,{
+"name": "APIRepository","key": "r","value": "66", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":1,"0":1,"27":1,"28":1,"29":2,"30":4,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":2,"8":2,"2":1,"3":1,"1":1}
+,
+"metricvalues":{"20":3,"21":0,"22":5,"23":0.0,"24":41,"25":0,"26":1,"0":1,"27":8,"28":28,"29":0.679,"30":0.857,"31":2,"32":0.542,"33":1,"34":23,"35":0,"36":2,"16":11,"37":0,"4":2,"8":66,"2":1,"3":1,"1":1}
+}
+]
+ }
+]
+ }
+;
+return ret;
+}
+var EQ_METRIC_MAP = {};
+EQ_METRIC_MAP["C3"] =0;
+EQ_METRIC_MAP["Complexity"] =1;
+EQ_METRIC_MAP["Coupling"] =2;
+EQ_METRIC_MAP["Lack of Cohesion"] =3;
+EQ_METRIC_MAP["Size"] =4;
+EQ_METRIC_MAP["Number of Highly Problematic Classes"] =5;
+EQ_METRIC_MAP["Number of Entities"] =6;
+EQ_METRIC_MAP["Number of Problematic Classes"] =7;
+EQ_METRIC_MAP["Class Lines of Code"] =8;
+EQ_METRIC_MAP["Number of External Packages"] =9;
+EQ_METRIC_MAP["Number of Packages"] =10;
+EQ_METRIC_MAP["Number of External Entities"] =11;
+EQ_METRIC_MAP["Efferent Coupling"] =12;
+EQ_METRIC_MAP["Number of Interfaces"] =13;
+EQ_METRIC_MAP["Number of Classes"] =14;
+EQ_METRIC_MAP["Afferent Coupling"] =15;
+EQ_METRIC_MAP["Weighted Method Count"] =16;
+EQ_METRIC_MAP["Normalized Distance"] =17;
+EQ_METRIC_MAP["Abstractness"] =18;
+EQ_METRIC_MAP["Instability"] =19;
+EQ_METRIC_MAP["Coupling Between Object Classes"] =20;
+EQ_METRIC_MAP["Access to Foreign Data"] =21;
+EQ_METRIC_MAP["Number of Fields"] =22;
+EQ_METRIC_MAP["Specialization Index"] =23;
+EQ_METRIC_MAP["Class-Methods Lines of Code"] =24;
+EQ_METRIC_MAP["Number of Children"] =25;
+EQ_METRIC_MAP["Depth of Inheritance Tree"] =26;
+EQ_METRIC_MAP["Number of Methods"] =27;
+EQ_METRIC_MAP["Response For a Class"] =28;
+EQ_METRIC_MAP["Lack of Tight Class Cohesion"] =29;
+EQ_METRIC_MAP["Lack of Cohesion of Methods"] =30;
+EQ_METRIC_MAP["Number of Static Fields"] =31;
+EQ_METRIC_MAP["Lack of Cohesion Among Methods(1-CAM)"] =32;
+EQ_METRIC_MAP["CBO App"] =33;
+EQ_METRIC_MAP["Simple Response For a Class"] =34;
+EQ_METRIC_MAP["Number of Static Methods"] =35;
+EQ_METRIC_MAP["CBO Lib"] =36;
+EQ_METRIC_MAP["Number of Overridden Methods"] =37;
+var EQ_SELECTED_CLASS_METRIC 		= "C3";
+var EQ_SELECTED_PACKAGE_METRIC 	= "C3";
+var EQ_SELECTED_PROJECT_METRIC 	= "Class Lines of Code";
+var EQ_CLASS_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_CLASS_METRIC];
+var EQ_PACKAGE_METRIC_INDEX	= EQ_METRIC_MAP[EQ_SELECTED_PACKAGE_METRIC];
+var EQ_PROJECT_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_PROJECT_METRIC];
+var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
+var EQ_CLASS_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Class Lines of Code","Weighted Method Count","Coupling Between Object Classes","Access to Foreign Data","Number of Fields","Specialization Index","Class-Methods Lines of Code","Number of Children","Depth of Inheritance Tree","Number of Methods","Response For a Class","Lack of Tight Class Cohesion","Lack of Cohesion of Methods","Number of Static Fields","Lack of Cohesion Among Methods(1-CAM)","CBO App","Simple Response For a Class","Number of Static Methods","CBO Lib","Number of Overridden Methods"];
+var EQ_PACKAGE_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Number of Entities","Class Lines of Code","Efferent Coupling","Number of Interfaces","Number of Classes","Afferent Coupling","Weighted Method Count","Normalized Distance","Abstractness","Instability"];
+var EQ_PROJECT_METRICS = ["Number of Highly Problematic Classes","Number of Entities","Number of Problematic Classes","Class Lines of Code","Number of External Packages","Number of Packages","Number of External Entities"];
+function EQ_GET_COLOR(d) {
+if(d.metrics)
+return EQ_COLOR_OF_LEVELS[d.metrics[EQ_CLASS_METRIC_INDEX]];
+if(d.pmetrics)
+return EQ_COLOR_OF_LEVELS[d.pmetrics[EQ_PACKAGE_METRIC_INDEX]];
+if(d.prmetrics)
+return EQ_COLOR_OF_LEVELS[d.prmetrics[EQ_PROJECT_METRIC_INDEX]];
+return EQ_COLOR_OF_LEVELS[0];
+}
